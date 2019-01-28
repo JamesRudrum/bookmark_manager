4e69e512f01ace3e939b3684356b5f4da6f99f20
James <james.rudrum@hyperisland.se>
2019-01-28 18:51:52 +0000

    implement view and model, Bookmark. Unit test for Bookmark.MVC pattern

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
 require 'sinatra/base'
+require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
   get '/' do
     'Rudrums Bookmark Manager'
+  end
+
+  get '/bookmarks' do
+    @bookmarks = Bookmark.all
+
+    erb(:index)
   end
 
   run if app_file == $0
---
+++ lib/bookmark.rb
@@ -0,0 +1,9 @@
+class Bookmark
+  def self.all
+    [
+      'www.reddit.com',
+      'www.livescore.com',
+      'www.github.com'
+    ]
+  end
+end
