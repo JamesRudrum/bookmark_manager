3d04d79d03e3fe887dc9a2ffaac2d3f6e3381033
James <james.rudrum@hyperisland.se>
2019-01-29 20:19:25 +0000

    added pg to gemfile. retreive bookmark tables from bookmark_manager database

--- README.md
+++ README.md
@@ -4,8 +4,13 @@
 User Stories:
 
 #1
-As a busy online person
+As a busy online user
 So that I can visit my regular sites quickly
 I would like to view a list of bookmarks
 
 <img width="767" alt="user story 1 domain model" src="https://user-images.githubusercontent.com/45013014/51850556-dff46080-2319-11e9-9bf9-88ce4e681550.png">
+
+#2
+As a time-pressed user
+So that I can save a website
+I would like to add the site's address and title to bookmark manager
--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,9 +1,15 @@
+require 'pg'
+
 class Bookmark
   def self.all
-    [
-      'www.reddit.com',
-      'www.livescore.com',
-      'www.github.com'
-    ]
+    con = PG.connect :dbname => 'bookmark_manager'
+    # use pg gem to connect to bookmark_manager database
+    bm = con.exec("SELECT * FROM bookmarks;")
+    # retrieve all bookmarks from the bookmark table in bookmark database
+    # this returns an array of hashes
+    # [{"id" => "1", "url", "www.google.com"}]
+    bm.map { |bookmarks| bookmarks['url'] }
+    # use map to create array
+    # can use map as "SELECT * FROM bookmarks;" returns an array of hashes
   end
 end
