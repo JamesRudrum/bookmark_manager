90e3acdfb6cc7d55cb147596b42987a5af7b56eb
James <james.rudrum@hyperisland.se>
2019-01-29 20:26:08 +0000

    updated comments to understand viewing the bookmark urls

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -5,11 +5,14 @@ class Bookmark
     con = PG.connect :dbname => 'bookmark_manager'
     # use pg gem to connect to bookmark_manager database
     bm = con.exec("SELECT * FROM bookmarks;")
-    # retrieve all bookmarks from the bookmark table in bookmark database
+    # select all bookmarks from the bookmark table in bookmark_manager database
     # this returns an array of hashes
     # [{"id" => "1", "url", "www.google.com"}]
     bm.map { |bookmarks| bookmarks['url'] }
     # use map to create array
     # can use map as "SELECT * FROM bookmarks;" returns an array of hashes
+    # bm.map { |bookmarks| bookmarks['url'] } for each element in enumerable
+    # do block. The block in this instance creates a bookmark by using the 'url'
+    # key from the hashes in the array.
   end
 end
