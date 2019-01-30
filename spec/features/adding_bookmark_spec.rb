0740efc7ef815dba1a2b6741b2cc90b28fb892c9
James <james.rudrum@hyperisland.se>
2019-01-30 17:32:57 +0000

    Feature test for adding bookmark

---
+++ spec/features/adding_bookmark_spec.rb
@@ -0,0 +1,9 @@
+feature 'Add Bookmarks' do
+  scenario 'check that a bookmark can be added to /bookmarks' do
+    bookmark_insert
+    visit('/bookmarks')
+    fill_in :url, with: 'www.skysports.com'
+    click_button 'Add Bookmark!'
+    expect(page).to have_content 'www.skysports.com'
+  end
+end
